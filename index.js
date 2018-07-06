54d37c09ec66dd31f6a66705c59550d14f9bdcfc
Chris Smith <chris.smith@ovoenergy.com>
2018-07-06 10:51:11 +0100

    Do iteration for individual points in Rust (inefficient)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { greet } from "./wasm_mandelbrot";
+import { iterations } from "./wasm_mandelbrot";
 
 var canvas = document.getElementById('mandelbrot-canvas');
 
@@ -7,34 +7,11 @@ canvas.width = 600;
 
 const ctx = canvas.getContext('2d');
 
-const next = (z, c) => {
-    return { re: c.re + z.re * z.re - z.im * z.im, im: c.im + 2 * z.re * z.im }
-}
-
-const mag = z => Math.sqrt(z.re * z.re + z.im * z.im)
-
-const maxIter = 500;
-
-const isIn = c => {
-    var i = 0;
-    var z = { re: 0, im: 0 }
-    while (mag(z) < 2 && i < maxIter) {
-        i++;
-        z = next(z, c)
-    }
-    return i;
-}
-
 for (var x = 0; x < 600; x++) {
     for (var y = 0; y < 600; y++) {
-        var z = { re: (x - 300) / 150, im: (y - 300) / 150 };
-        var iters = isIn(z)
-        if (iters < maxIter) {
-            var style = 'rgb(' + Math.min(255, Math.floor(255 * iters / 20)) + ', 0, 0)';
-            ctx.fillStyle = style;
-            ctx.fillRect(x, y, 1, 1);
-        }
+        var iters = iterations((x - 300) / 150, (y - 300) / 150);
+        var style = 'rgb(' + Math.min(255, Math.floor(255 * iters / 20)) + ', 0, 0)';
+        ctx.fillStyle = style;
+        ctx.fillRect(x, y, 1, 1);
     }
 }
-
-setTimeout(() => greet("Mandelbrot set"), 1000);
--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,49 @@
 extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
+use std::f32;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[derive(Clone, Copy)]
+struct Complex {
+    pub re: f32,
+    pub im: f32
+}
+
+impl Complex {
+    pub fn new(re: f32, im: f32) -> Complex {
+        Complex { re, im }
+    }
+
+    pub fn zero() -> Complex {
+        Complex::new(0.0, 0.0)
+    }
+
+    pub fn squared(&self) -> Complex {
+        Complex::new(
+            self.re * self.re - self.im * self.im,
+            2.0 * self.re * self.im
+        )
+    }
+
+    pub fn plus(&self, c: Complex) -> Complex {
+        Complex::new(self.re + c.re, self.im + c.im)
+    }
+
+    pub fn magnitude(&self) -> f32 {
+        (self.re * self.re + self.im * self.im).sqrt()
+    }
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn iterations(re: f32, im: f32) -> u8 {
+    let c: Complex = Complex::new(re, im);
+    let mut z = Complex::zero();
+    let mut n = 0;
+
+    while z.magnitude() < 2.0 && n < 100 {
+        z = z.squared().plus(c);
+        n = n + 1;
+    }
+
+    n
 }
