5f833fc667c9395842f3a3a2d79cfd34b336421f
Chris Smith <chris.smith@ovoenergy.com>
2018-07-06 14:52:49 +0100

    Add click event listener to canvas and zoom code

--- index.js
+++ index.js
@@ -13,22 +13,33 @@ const ctx = canvas.getContext('2d');
 
 const image = Image.new(width, height);
 
-const setPtr = image.calculate(-2.1, 0.6, -1.2, 1.2);
-const set = new Uint8Array(memory.buffer, setPtr, width * height);
+const render = (re_min, re_max, im_min, im_max) => {
+    console.log("Rendering (" + re_min.toFixed(3) + ", " + im_min.toFixed(3) + ") to (" + re_max.toFixed(3) + ", " + im_max.toFixed(3) + ")");
 
-for (var x = 0; x < width; x++) {
-    for (var y = 0; y < height; y++) {
-        var style = 'rgb(' + Math.min(255, Math.floor(255 * set[x * width + y] / 20)) + ', 0, 0)';
-        ctx.fillStyle = style;
-        ctx.fillRect(x, y, 1, 1);
+    const setPtr = image.calculate(re_min, re_max, im_min, im_max);
+    const set = new Uint8Array(memory.buffer, setPtr, width * height);
+
+    for (var x = 0; x < width; x++) {
+        for (var y = 0; y < height; y++) {
+            var style = 'rgb(' + Math.min(255, Math.floor(255 * set[x * width + y] / 20)) + ', 0, 0)';
+            ctx.fillStyle = style;
+            ctx.fillRect(x, y, 1, 1);
+        }
     }
 }
 
+render(-2.0, 2.0, -2.0, 2.0);
+
+var anchorPoint = null;
+
 canvas.addEventListener('click', e => {
-    let x = e.offsetX;
-    let y = e.offsetY;
-    console.log("On canvas: (" + x + ", " + y + ")");
-    let re = image.x_to_real(x);
-    let im = image.y_to_imaginary(y);
-    console.log("On complex plane: (" + re + ", " + im + ")");
+    if (anchorPoint === null) {
+        anchorPoint = { re: image.x_to_real(e.offsetX), im: image.y_to_imaginary(e.offsetY) };
+    } else {
+        let re = image.x_to_real(e.offsetX);
+        let im = image.y_to_imaginary(e.offsetY);
+
+        render(Math.min(re, anchorPoint.re), Math.max(re, anchorPoint.re), Math.min(im, anchorPoint.im), Math.max(im, anchorPoint.im))
+        anchorPoint = null;
+    }
 })
